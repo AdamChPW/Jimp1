ec2ea3bbdb1edb96bc0a1d7037a74a7fec44035a
Adam Chmielewski <01206754@pw.edu.pl>
2025-12-28 13:42:30 +0100

    Usuwanie zbednych plików

--- ItJustWorks.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void main(void){
-    printf("Napisalem to w CMD :\n");
-    int i = 0;
-    for(i=0;i<100; i++)
-        printf("%d\n", i);
-}
--- main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char** argv){
-
-    FILE *in = argc > 1 ? fopen(argv[1],"r") : stdin;
-    return 0;
-}
