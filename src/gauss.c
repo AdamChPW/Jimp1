bb1973bff70615100a4b54c717305378108b8303
Adam Chmielewski <01206754@pw.edu.pl>
2026-01-06 14:10:59 +0100

    Moja wersja wyboru elementu glownego

--- src/gauss.c
+++ src/gauss.c
@@ -10,26 +10,27 @@ int eliminate(Matrix *mat, Matrix *b){
 	c = 0;
 	r = 0;
 	double x;
-	const double EPS = 1e-12;
 	while (r < mat->r - 1 && c < mat->c) {
 		maxr = r;
-		if (fabs(mat->data[maxr][c]) < EPS) {
+		for(i = r + 1; i< mat->r; i++)
+			if(mat->data[i][c] > mat->data[maxr][c])
+				maxr = i;
+		if (fabs(mat->data[maxr][c]) == 0) {
 			return 1;
 		}
 		if (maxr != r) {
+			double tmp;
 			for (j = c; j < mat->c; j++) {
-				double tmp;
 				tmp = mat->data[r][j];
 				mat->data[r][j] = mat->data[maxr][j];
 				mat->data[maxr][j] = tmp;
 			}
-			double tmpb;
-			tmpb = b->data[r][0];
+			tmp = b->data[r][0];
 			b->data[r][0] = b->data[maxr][0];
-			b->data[maxr][0] = tmpb;
+			b->data[maxr][0] = tmp;
 		}
 		for (i = r + 1; i < mat->r; i++) {
-			if (fabs(mat->data[r][c]) > EPS) {
+			if (fabs(mat->data[r][c]) != 0) {
 				x = mat->data[i][c] / mat->data[r][c];
 				for (j = 0; j < mat->c; j++) {
 					mat->data[i][j] -= x * mat->data[r][j];
